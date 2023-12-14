a5fdc6930bbf37f13dc26fe55a8f08e54c60843e
Annie Nusbaum <annie.nusbaum.83@gmail.com>
2023-12-14 14:57:35 -0500

    initial commit

---
+++ PlaylistProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PlaylistProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
